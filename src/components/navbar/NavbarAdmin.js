5013e131a2001bb8e88f6095467d26c10f5c8629
caio <zhj380@gmail.com>
2023-10-20 03:00:03 -0300

    caio

--- src/components/navbar/NavbarAdmin.js
+++ src/components/navbar/NavbarAdmin.js
@@ -1,4 +1,4 @@
-// Chakra Imports
+
 import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Link, Text, useColorModeValue } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
