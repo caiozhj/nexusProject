aa97b1f7d45545391733219049101a88c9afc462
lcas21 <lucassanciro@gmail.com>
2024-07-15 13:15:11 -0300

    lucas

--- src/views/admin/contabilidade/plano-contas/modalReservas.js
+++ src/views/admin/contabilidade/plano-contas/modalReservas.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from "react";
 import {
   Box,
   Heading,
@@ -9,97 +9,192 @@ import {
   Textarea,
   Button,
   useToast,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
+import axios, { Axios } from "axios";
+import { postCriarPix } from "api/service";
+
+// const api = axios.create({
+//   baseURL: "https://api.mercadopago.com",
+// });
+
+// api.interceptors.request.use(async (config) => {
+//   const token = "APP_USR-62c3699a-4e7e-44f7-b2da-dbfd2c499179";
+//   config.headers.Authorization = `Bearer ${token}`
+
+//   return config
+// });
+
+// const formReducer = (state, event) => {
+//   return {
+//     ...state,
+//     [event.name]: event.value
+//   }
+// }
+
+const MOCK_BODY = {
+  transaction_amount: 4,
+  description: "Pagamento de teste v05",
+  paymentMethodId: "pix",
+  email: "gersoncafilho2@gmail.com",
+  identificationType: "CPF",
+  number: "12345678909",
+};
 
 export default function ModalPagamentoPendente() {
-  const [formData, setFormData] = useState({
-    date: '',
-    activity: '',
-    status: '',
-    description: '',
-  });
 
-  const toast = useToast();
+  const [ticketUrl, setTicketUrl] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handlePostCriarPix = () => {
+    postCriarPix(MOCK_BODY).then(
+      response => {
+        console.log("response")
+        console.log(response.data)
+      }
+    )
   };
+  // const [formData, setFormData] = useState({
+  //   date: "",
+  //   activity: "",
+  //   status: "",
+  //   description: "",
+  // });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aqui você pode adicionar a lógica para enviar os dados do formulário para o backend
-    toast({
-      title: 'Pagamento pendente adicionado!',
-      description: `Atividade de ${formData.activity} com status ${formData.status} para a data ${formData.date} foi adicionada como pendente.`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    });
-    // // Pode adicionar aqui a lógica para limpar o formulário após o envio
-    // setFormData({
-    //   date: '',
-    //   activity: '',
-    //   status: 'Pendente',
-    //   description: '',
-    // });
-  };
+  // const toast = useToast();
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // };
+
+  // const [formData, setFormData]= useReducer(formReducer, {})
+
+  // const handleChange = event => (
+  //  setFormData ({
+  //   name: event.target.name,
+  //   value: event.target.value
+  //  })
+  // )
+
+  // const handleSubmit = (event) => {
+
+  //   event.preventDefault()
+  //   // e.preventDefault();
+  //   // Aqui você pode adicionar a lógica para enviar os dados do formulário para o backend
+  //   // toast({
+  //   //   title: "Pagamento pendente adicionado!",
+  //   //   description: `Atividade de ${formData.activity} com status ${formData.status} para a data ${formData.date} foi adicionada como pendente.`,
+  //   //   status: "success",
+  //   //   duration: 5000,
+  //   //   isClosable: true,
+
+  //   const body = {
+  //     "transaction_amount": 10,
+  //     "paymant_method_id": "pix",
+  //   }
+  //   api.post ("v1/payments", body).then(response => {
+  //   }).catch(err => {
+  //     // alert(err)
+  //   })
+
+  //   console.log(formData)
+  //   };
+  // // Pode adicionar aqui a lógica para limpar o formulário após o envio
+  // setFormData({
+  //   date: '',
+  //   activity: '',
+  //   status: 'Pendente',
+  //   description: '',
+  // });
 
   return (
     <Box p={5} bg="white" borderRadius="lg" boxShadow="md" mx="auto">
       <Heading as="h3" size="lg" mb={6} textAlign="center">
         Adicionar Pagamento Pendente
       </Heading>
-      <form onSubmit={handleSubmit}>
-        <FormControl id="date" mb={4} isRequired>
-          <FormLabel>Data</FormLabel>
-          <Input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl id="activity" mb={4} isRequired>
+      <div>
+      {MOCK_BODY.transaction_amount}
+      </div>
+      {/* <form>
+        <FormControl mb={4}>
           <FormLabel>Nome</FormLabel>
           <Input
             type="text"
-            name="activity"
-            value={formData.activity}
-            onChange={handleChange}
-            placeholder="Nome do serviço realizado"
-          />
+            name="nome"
+            placeholder="Nome"
+            // onChange={handleChange}
+          >
+            {MOCK_BODY.transaction_amount}
+          </Input>
         </FormControl>
-        <FormControl id="description" mb={4}>
-          <FormLabel>Descrição ou Motivo</FormLabel>
-          <Textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            placeholder="Descrição detalhada ou motivo do recebimento"
-            size="md" // Ajuste o tamanho conforme necessário
-            resize="vertical" // Permite redimensionamento vertical
-          />
+        <FormControl mb={4}>
+          <FormControl mb={4}>
+            <FormLabel>Nome</FormLabel>
+            <Input
+              type="name"
+              name="nome"
+              placeholder="Nome"
+              // onChange={handleChange}
+            >
+              {MOCK_BODY.description}
+            </Input>
+          </FormControl>
         </FormControl>
-        <FormControl id="status" mb={4} isRequired>
+        <FormControl mb={4}>
+          <FormControl mb={4}>
+            <FormLabel>Nome</FormLabel>
+            <Input
+              type="name"
+              name="nome"
+              placeholder="Nome"
+              // onChange={handleChange}
+            >
+              {MOCK_BODY.email}
+            </Input>
+          </FormControl>
+          <FormControl mb={4}>
+            <FormLabel>Nome</FormLabel>
+            <Input
+              type="name"
+              name="nome"
+              placeholder="Nome"
+              // onChange={handleChange}
+            >
+              {MOCK_BODY.number}
+            </Input>
+          </FormControl>
+        </FormControl> */}
+        {/* <FormControl id="status" mb={4} isRequired>
           <FormLabel>Status</FormLabel>
           <Select
             name="status"
-            value={formData.status}
-            onChange={handleChange}
+            value={formData.s  tatus}
+            // onChange={handleChange}
             placeholder="Selecione o status"
           >
             <option value="Pendente">Pendente</option>
             <option value="Pago">Pago</option>
           </Select>
-        </FormControl>
-        <Button colorScheme="blue" type="submit" width="full">
-          Adicionar Pagamento Pendente
+        </FormControl> */}
+        {/* <div>
+          <label>Nome</label>
+          <input onChange= {handleChange} name="nome" />
+        </div> */}
+
+        {/* <div>
+          <label>Nome</label>
+          <imput onChange= {handleChange} name="nome" />
+        </div> */}
+        {/* <Button colorScheme="blue" onClick={handlePostCriarPix} width="full">
+          Enviar combrança
+        </Button> */}
+      {/* </form> */}
+
+      <Button colorScheme="blue" onClick={handlePostCriarPix} width="full">
+          Enviar combrança
         </Button>
-      </form>
     </Box>
   );
 }
